5993893091cd071a798ddd1d58bb0339ee72a998
Andres Recalde <andres.recalde@lacolombe.net>
2016-12-05 11:42:37 -0500

    fixed coffee weight/extension calcs

--- models/aggregates/warehouse_invoice_aggregates.sql
+++ models/aggregates/warehouse_invoice_aggregates.sql
@@ -7,8 +7,8 @@ select
 	sum(il.quantity_backordered) as total_quantity_backordered,
 	sum(il.extension) as total_extension,
 	sum(il.total_weight) as total_weight,
-	sum((il.sku like '%C')::integer * il.total_weight) as total_coffee_weight,
-	sum((il.sku like '%C')::integer * il.extension) as total_coffee_extension
+	sum((il.sku like 'C%')::integer * il.total_weight) as total_coffee_weight,
+	sum((il.sku like 'C%')::integer * il.extension) as total_coffee_extension
 
 from {{ref('warehouse_base_invoice_items')}} il
 group by 1,2
