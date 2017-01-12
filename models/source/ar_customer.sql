b49c7448ef708e87ff98e3b5b2dcd96dd34c8f0d
Andres Recalde <andres.recalde@lacolombe.net>
2017-01-11 23:07:47 -0500

    added terms code

--- models/source/ar_customer.sql
+++ models/source/ar_customer.sql
@@ -7,7 +7,7 @@ agingcategory3 as overdue_balance_90_day,
 agingcategory4 as overdue_balance_120_day,
 avgdaysoverdue as average_days_overdue,
 currentbalance as current_balance,
-termscode as payment_terms,
+termscodedesc as payment_terms,
 avgdayspaymentinvoice as average_days_to_payment,
 addressline1 as address_line_1,
 case
@@ -22,4 +22,5 @@ coalesce(countrycode, 'USA') as country,
 zipcode as zip,
 taxschedule as tax
 
-from saleslogix.ar_customer
+from saleslogix.ar_customer c
+left join saleslogix.ap_termscode t on c.termscode = t.termscode
