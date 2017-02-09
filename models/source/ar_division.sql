d144b19a10e68fd35e1b2a598f7eb9326a6e96f9
Andres Recalde <andres.recalde@lacolombe.net>
2017-02-09 15:12:08 -0500

    fixed division

--- models/source/ar_division.sql
+++ models/source/ar_division.sql
@@ -1,2 +1,8 @@
-select ardivisionno as division_code, ardivisiondesc as division
+select ardivisionno as division_code, 
+case ardivisionno
+	when "01" then "Philly"
+	when "30" then "D.C."
+	when "20" then "New York"
+	else ardivisiondesc 
+end as division
 from dbo.ar_division
