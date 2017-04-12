2f8e83b1f40b3ac742c26b0f143e2d188aa03a46
Andres Recalde <andres.recalde@lacolombe.net>
2017-04-12 15:55:00 -0400

    made invalid dates null ap invoices

--- models/source/accounts_payable_invoices.sql
+++ models/source/accounts_payable_invoices.sql
@@ -1,10 +1,16 @@
 select 
 invoiceno as invoice_number, 
-invoicedate as invoice_date, 
+case invoicedate 
+	when '1753-01-01' then null
+	else invoicedate
+end as invoice_date, 
 vendorno as vendor_number, 
 invoiceamt as invoice_amount, 
 checkno as check_number, 
 checktype as check_type, 
-checkdate as check_date, 
+case checkdate 
+  when '1753-01-01' then null
+  else checkdate
+end as check_date, 
 balance as balance
 from dbo.ap_openinvoice 
