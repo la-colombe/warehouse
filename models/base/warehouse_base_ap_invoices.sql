a68a4c7a22b9d687acf9433afdb379ba738520ba
Andres Recalde <andres.recalde@lacolombe.net>
2017-04-12 16:02:44 -0400

    added check type

--- models/base/warehouse_base_ap_invoices.sql
+++ models/base/warehouse_base_ap_invoices.sql
@@ -5,5 +5,6 @@ vendor_number,
 invoice_amount, 
 check_number, 
 check_date, 
+check_type,
 balance
 from {{ref('accounts_payable_invoices')}} 
--- models/tables/warehouse_ap_invoices.sql
+++ models/tables/warehouse_ap_invoices.sql
@@ -4,6 +4,7 @@ invoice_date,
 vendor_number, 
 invoice_amount, 
 check_number, 
+check_type,
 check_date, 
 balance
 from {{ref('warehouse_base_ap_invoices')}} 
