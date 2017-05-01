2390580d17cdace93d734d5bea6c14b13de06592
Andres Recalde <andres.recalde@lacolombe.net>
2017-05-01 10:53:35 -0400

    added Norton warehouse name mapping

--- models/source/im_warehouse.sql
+++ models/source/im_warehouse.sql
@@ -1,2 +1,6 @@
-select warehousecode as warehouse_code, warehousedesc as warehouse_name
+select warehousecode as warehouse_code, 
+case warehousedesc 
+	when 'La Colombe Norton Shores' then 'Norton Shores'
+	else warehousedesc
+end as warehouse_name
 from dbo.im_warehouse w
