6719b79890cd13422aa2da53d8ece4185fb12d0c
Andres Recalde <andres.recalde@lacolombe.net>
2017-05-22 15:40:10 -0400

    changed join for most recent check

--- models/tables/warehouse_ap_invoices.sql
+++ models/tables/warehouse_ap_invoices.sql
@@ -17,4 +17,4 @@ c.bank_code,
 c.check_comment
 from {{ref('warehouse_base_ap_invoices')}} i
 left join {{ref('warehouse_ap_invoice_checks')}} ic on ic.invoice_number = i.invoice_number and ic.invoice_sequence_number = i.header_sequence_number
-left join {{ref('warehouse_base_ap_checks')}} c on c.invoice_number = i.invoice_number and c.invoice_sequence_number = i.header_sequence_number and row_number = 1
+left join {{ref('warehouse_base_ap_checks')}} c on c.invoice_number = i.invoice_number and c.vendor_number = i.vendor_number and c.division = i.division and row_number = 1
