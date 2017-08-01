a2351a9e30ddfafc386f11a0eb7e7f4b5b5607f7
Andres Recalde <andres.recalde@lacolombe.net>
2017-08-01 12:12:05 -0400

    fixed sales order warehouse

--- models/base/warehouse_base_sales_order_items.sql
+++ models/base/warehouse_base_sales_order_items.sql
@@ -3,9 +3,10 @@ sales_order_number,
 line_number,
 sku,
 item_name,
-warehouse,
+warehouse_name as warehouse,
 original_quantity,
 revised_quantity,
 shipped_quantity,
 backordered_quantity
-from {{ref('sales_order_history_detail')}}
+from {{ref('sales_order_history_detail')}} so
+left join {{ref('im_warehouse')}} w on w.warehouse_code = so.warehouse_code
