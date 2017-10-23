a27c10132b4678f034eb858c4cb90db324d7689a
Andres Recalde <andres.recalde@lacolombe.net>
2017-10-23 15:51:26 -0400

    changed code to name

--- models/tables/warehouse_item_transactions.sql
+++ models/tables/warehouse_item_transactions.sql
@@ -2,9 +2,10 @@ select
 entry_number,
 customer_code,
 sku,
-warehouse_code,
+warehouse_name,
 transaction_date,
 sent_date,
 transaction_quantity,
 transaction_type
 from {{ref('im_item_transactions')}} t
+join {{ref('im_warehouse')}} w on w.warehouse_code = t.warehouse_code
