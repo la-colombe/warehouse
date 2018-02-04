2c18cd05c3eda066b88e31505f0e985accd94962
Andres Recalde <andres.recalde@lacolombe.net>
2018-02-04 14:11:02 -0500

    changed weekly def

--- models/tables/warehouse_accounts.sql
+++ models/tables/warehouse_accounts.sql
@@ -72,9 +72,9 @@ case
 	else NULL
 end as churn_date,
 
-datediff(week, aa.second_paid_coffee_invoice_date, aa.most_recent_invoice_date) + 1 as weeks_active,
-round((aa.total_core_extension / nullif(datediff(week, aa.second_paid_coffee_invoice_date, aa.most_recent_invoice_date) + 1, 0))::decimal(16,2),2) as average_weekly_core_revenue,
-round((aa.total_core_weight/ nullif(datediff(week, aa.second_paid_coffee_invoice_date, aa.most_recent_invoice_date) + 1, 0))::decimal(16,2),2) as average_weekly_core_volume,
+datediff(week, aa.first_core_invoice_date, aa.most_recent_core_invoice_date) + 1 as weeks_active,
+round((aa.total_core_extension / nullif(datediff(week, aa.first_core_invoice_date, aa.most_recent_core_invoice_date) + 1, 0))::decimal(16,2),2) as average_weekly_core_revenue,
+round((aa.total_core_weight/ nullif(datediff(week, aa.first_core_invoice_date, aa.most_recent_core_invoice_date) + 1, 0))::decimal(16,2),2) as average_weekly_core_volume,
 round((aa.total_coffee_extension / nullif(aa.total_coffee_weight, 0))::decimal(16,2),2) as average_coffee_price,
 
 coalesce(aaa.total_value, 0) as total_asset_value,
