3dc4b1bcf8ddd8623fe9e6bebc57da979bfbb6d2
Andres Recalde <andres.recalde@lacolombe.net>
2018-06-19 08:55:10 -0400

    added invoices

--- models/tables/warehouse_ap_invoices.sql
+++ models/tables/warehouse_ap_invoices.sql
@@ -4,17 +4,17 @@ i.invoice_number,
 i.header_sequence_number,
 i.invoice_date, 
 i.vendor_number, 
-ia.extension as invoice_amount, 
+i.invoice_amount,
+i.shipping_amount,
 i.invoice_comment,
 i.vendor_name,
 i.purchase_order_number,
 i.days_before_due,
 nvl(ic.amount_paid, 0) as amount_paid,
-nvl(ia.extension,0) - nvl(ic.amount_paid,0) as balance,
+nvl(i.invoice_amount,0) - nvl(ic.amount_paid,0) as balance,
 i.created_at,
 i.created_by,
 i.updated_at,
 i.updated_by
 from {{ref('warehouse_base_ap_invoices')}} i
 left join {{ref('warehouse_ap_invoice_checks')}} ic on ic.invoice_number = i.invoice_number and ic.invoice_sequence_number = i.header_sequence_number
-left join {{ref('warehouse_ap_invoice_aggregates')}} ia on ia.invoice_number = i.invoice_number and ia.header_sequence_number = i.header_sequence_number
