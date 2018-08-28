f5da6b1721cf0fa513a59d22a50f9026f43ea61b
Jim Roddy <james.roddy@lacolombe.net>
2018-08-28 10:02:38 -0400

    Pulled in invoice level terms on AP invoices

--- models/base/warehouse_base_ap_invoices.sql
+++ models/base/warehouse_base_ap_invoices.sql
@@ -8,11 +8,13 @@ i.vendor_number,
 i.invoice_amount,
 i.shipping_amount,
 invoice_comment,
+i.terms_code,
 vendor_name,
-v.days_before_due,
+NVL(apt.days_before_due,v.days_before_due) as days_before_due,
 i.created_at,
 i.created_by,
 i.updated_at,
 i.updated_by
 from {{ref('ap_invoice_history_header')}} i
 left join {{ref('warehouse_base_ap_vendors')}} v on v.division = i.division and v.vendor_number = i.vendor_number
+left join {{ref('ap_terms')}} apt on apt.terms_code = i.terms_code
