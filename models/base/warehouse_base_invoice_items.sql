7685cf6de5ded16672f12657ccb3c60e1bf006b9
Jim Roddy <james.roddy@lacolombe.net>
2018-10-01 16:02:57 -0400

    added invoice level warehosue to invoice items

--- models/base/warehouse_base_invoice_items.sql
+++ models/base/warehouse_base_invoice_items.sql
@@ -33,12 +33,13 @@ SELECT
   i.freight as invoice_freight,
   i.comment,
   i.created_at,
-i.created_by,
-i.updated_at,
-i.updated_by,
+  i.created_by,
+  i.updated_at,
+  i.updated_by,
   i.account_invoice_number,
- il.quantity * p.weight as total_weight,
- w.warehouse_name
+  il.quantity * p.weight as total_weight,
+  w.warehouse_name,
+  i.warehouse_name as invoice_warehouse_name
 
 from {{ref('invoice_history_detail')}} il
 join {{ref('warehouse_base_invoices')}} i on i.unique_invoice_id = il.unique_invoice_id
