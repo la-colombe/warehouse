855860d004ab32c8ed3c119e4a0df0a3f0522bd0
Jim Roddy <james.roddy@lacolombe.net>
2019-09-05 16:20:17 -0400

    Removed unnecessary slx user joins

--- models/source/mapping/slx_c_account.sql
+++ models/source/mapping/slx_c_account.sql
@@ -2,7 +2,9 @@ select distinct
 
 accountid as account_id,
 accountmanagerid2 as sales_rep_id,
+creator as sales_rep_name,
 accountmanagerid3 as secondary_sales_rep_id,
+co_creator as secondary_sales_rep_name,
 call_frequency,
 customertype as group_code,
 newminvol as min_vol,
