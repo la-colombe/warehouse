77f59bca876cb1ad7ec2a5c24fc42be75faf797c
JimLaColombe <james.roddy@lacolombe.net>
2021-08-17 18:06:00 -0400

    Added promise date to sales_order_items

--- models/source/ar-so/so_sales_order_history_detail.sql
+++ models/source/ar-so/so_sales_order_history_detail.sql
@@ -3,6 +3,10 @@ d.salesorderno as sales_order_number,
 d.linekey as line_number,
 d.itemcode as sku,
 d.itemcodedesc as item_name,
+  case d.promisedate
+    when '1753-01-01' then null
+    else d.promisedate::date
+  end as promise_date,
 w.warehouse_name as warehouse,
 d.quantityorderedoriginal as original_quantity,
 d.quantityorderedrevised as revised_quantity,
