5c158a1d760d85e18636e08acf6bbf81a6748913
JimLaColombe <james.roddy@lacolombe.net>
2021-12-15 11:04:38 -0500

    Unmaterialized production tables

--- models/source/im-bm-ci/bm_production_history_detail.sql
+++ models/source/im-bm-ci/bm_production_history_detail.sql
@@ -1,12 +1,3 @@
-{{
-  config({
-    "materialized" : "table",
-    "post-hook" : [
-      "grant select on table {{this}} to group non_gl_read_only"
-      ]
-    })
-}}
-
 select 
 	
 	d.productionno,
