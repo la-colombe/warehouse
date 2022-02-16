a40d46b56684910225a4f7ef3fceb7c472f1be64
chris-schmid <chris.schmid@lacolombe.net>
2022-02-16 18:37:01 -0500

    Migrated sysdba.accountproduct to new accountasset table

--- models/source/ar-so/slx_assets.sql
+++ models/source/ar-so/slx_assets.sql
@@ -3,18 +3,12 @@ select
 	A.accountid as account_id,
 	A.ActualID as sku, 
 	A.productname as product_name,
-	A.UserField1 as date_purchased, 
+	A.Purchase_Date as date_purchased, 
 	A.SerialNumber as serial_number,
-	case
-	  when (regexp_replace(A.UserField2,'[^0-9.]') in ('.','') or regexp_count(A.UserField2,'\\.') > 1) then null
-	  else regexp_replace(A.UserField2,'[^0-9.]')
-	end value,
+	A.HistoricalValue as value,
 	A.UserField3 as Status,
 	A.UserField4 as date_moved, 
 	A.UserField5 as vendor,
  	A.UserField6 as class,  
-	case
-	  when (regexp_replace(A.UserField8,'[^0-9.]') in ('.','') or regexp_count(A.UserField8,'\\.') > 1) then null
-	  else regexp_replace(A.UserField8,'[^0-9.]')
-	end invested_value
-from sysdba.accountproduct A
+	A.InvestValue as invested_value
+from sysdba.accountasset A
