506a9ba0f682568e04baeffbab3109c5a97255ce
JimLaColombe <james.roddy@lacolombe.net>
2022-03-22 19:36:49 -0400

    Added warehouse and gl fields to item table

--- models/source/im-bm-ci/ci_item.sql
+++ models/source/im-bm-ci/ci_item.sql
@@ -14,6 +14,14 @@ i.primaryvendorno as vendor,
 i.standardunitcost as unit_cost,
 i.pricecode as price_code,
 i.datecreated + (nullif(i.timecreated, '')::DECIMAL(7,5) || ' hours')::interval as created_at,
-i.dateupdated + (nullif(i.timeupdated, '')::DECIMAL(7,5) || ' hours')::interval as updated_at
+i.dateupdated + (nullif(i.timeupdated, '')::DECIMAL(7,5) || ' hours')::interval as updated_at,
+i.defaultwarehousecode as default_warehouse_code,
+pl.inventoryacctkey as inventory_gl_id,
+pl.costofgoodssoldacctkey as cogs_gl_id,
+pl.salesincomeacctkey as sales_income_gl_id,
+pl.returnsacctkey as return_gl_id,
+pl.adjustmentacctkey as adjustment_gl_id,
+pl.purchaseacctkey as purchase_gl_id
+
 from dbo.ci_item i
 left join dbo.im_productline pl on  pl.productline = i.productline
--- models/tables/warehouse_products.sql
+++ models/tables/warehouse_products.sql
@@ -1,15 +1,28 @@
 select
-sku, 
-created_at,
-name, 
-product_line, 
-unit_price, 
-product_type,
-unit_of_measure, 
-weight,  
-tax_class, 
-unit_cost,
-vendor,
-updated_at
+i.sku, 
+i.created_at,
+i.name, 
+i.product_line, 
+i.unit_price, 
+i.product_type,
+i.unit_of_measure, 
+i.weight,  
+i.tax_class, 
+i.unit_cost,
+i.vendor,
+i.updated_at,
+i.default_warehouse_code,
+gli.full_account_number as inventory_gl_account_number,
+glc.full_account_number as cogs_gl_account_number,
+gls.full_account_number as sales_income_gl_account_number,
+glr.full_account_number as return_gl_account_number,
+gla.full_account_number as adjustment_gl_account_number,
+glp.full_account_number as purchase_gl_account_number
 
-from {{ref('ci_item')}}
+from {{ref('ci_item')}} i
+	left join {{ref('gl_account')}} gli on i.inventory_gl_id = gli.id
+	left join {{ref('gl_account')}} glc on i.cogs_gl_id = glc.id
+	left join {{ref('gl_account')}} gls on i.sales_income_gl_id = gls.id
+	left join {{ref('gl_account')}} glr on i.return_gl_id = glr.id
+	left join {{ref('gl_account')}} gla on i.adjustment_gl_id = gla.id
+	left join {{ref('gl_account')}} glp on i.purchase_gl_id = glp.id
