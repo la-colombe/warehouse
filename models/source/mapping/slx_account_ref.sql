3ef3ed2a4d643434e3db7c8c251c3d1d844dcc75
Rachel.Ward <rachel.ward@RachelWards-MacBook-Pro.local>
2022-10-31 17:31:51 -0400

    removed accounts without assets

--- models/source/mapping/slx_account_ref.sql
+++ models/source/mapping/slx_account_ref.sql
@@ -1,9 +1,17 @@
-select distinct
-
-acct_id as customer_code,
-max(accountid) as account_id,
-max(acct_division) as division,
-max(comp_code) as company_code
+with assetonly as
+(select accountid,
+count(aa.accountid) as asset_rows
 
 from {{source('sysdba','dl_acct_ref')}} dl
+left join {{source('sysdba','accountasset')}} aa using(accountid)
 group by 1
+having asset_rows >= 1)
+
+select distinct
+acct_id as customer_code,
+accountid as account_id,
+acct_division as division,
+comp_code as company_code
+
+from {{source('sysdba','dl_acct_ref')}} dl
+    join assetonly ao using(accountid)
