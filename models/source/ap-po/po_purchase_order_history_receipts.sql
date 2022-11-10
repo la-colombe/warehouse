13ee2c6e58b76522450f1fefb50baff1e8e8893e
chris-schmid <chris.schmid@lacolombe.net>
2022-11-10 11:09:06 -0500

    Deprecated warehouse code 038 in favor of 049

--- models/source/ap-po/po_purchase_order_history_receipts.sql
+++ models/source/ap-po/po_purchase_order_history_receipts.sql
@@ -1,26 +1,44 @@
+with po_receipthistorydetail as (
+	select
+		headerseqno,
+		receipttype as receipt_type,
+		receiptno as receipt_number,
+		linekey as purchase_order_line_number,
+		purchaseorderno as purchase_order_number,
+		--Warehouse 038 and 049 both represent the same Garfield location. 038 is deprecated in favor of 049
+	  case warehousecode 
+	    when '038' then '049' 
+	    else warehousecode 
+	  end as warehouse_code,  
+		itemcode as sku,
+		quantityreceived * (case unitofmeasureconvfactor when 0 then 1 else unitofmeasureconvfactor end) as quantity,
+		quantityreceived as quantity_in_ordered_uom
+	from {{source('sage','po_receipthistorydetail')}}
+)
+
 select 
-d.receipttype as receipt_type,
-d.receiptno as receipt_number,
-linekey as purchase_order_line_number,
-d.purchaseorderno as purchase_order_number,
-itemcode as sku,
-  case receiptdate 
-    when '1753-01-01' then null
-    else receiptdate::date
-  end as recieved_at,
-  case h.transactiondate
-	when '1753-01-01' then null
-	else h.transactiondate::date
-  end as transaction_date,
-quantityreceived * (case unitofmeasureconvfactor when 0 then 1 else unitofmeasureconvfactor end) as quantity,
-quantityreceived as quantity_in_ordered_uom,
-w.warehouse_name as warehouse,
-datecreated + (nullif(timecreated, '')::DECIMAL(7,5) || ' hours')::interval as created_at,
-cu.full_name as created_by,
-dateupdated + (nullif(timeupdated, '')::DECIMAL(7,5) || ' hours')::interval as updated_at,
-uu.full_name as updated_by
-from {{source('sage','po_receipthistorydetail')}} d
-join {{source('sage','po_receipthistoryheader')}} h on h.purchaseorderno = d.purchaseorderno and h.headerseqno = d.headerseqno and h.receiptno = d.receiptno
-left join {{ref('sy_user')}} cu on cu.user_key = usercreatedkey
-left join {{ref('sy_user')}} uu on uu.user_key = userupdatedkey
-left join {{ref('im_warehouse')}} w on w.warehouse_code = d.warehousecode
+	d.receipt_type,
+	d.receipt_number,
+	d.purchase_order_line_number,
+	d.purchase_order_number,
+	d.sku,
+	case h.receiptdate 
+		when '1753-01-01' then null
+		else h.receiptdate::date
+	end as recieved_at,
+	case h.transactiondate
+		when '1753-01-01' then null
+		else h.transactiondate::date
+	end as transaction_date,
+	d.quantity,
+	d.quantity_in_ordered_uom,
+	w.warehouse_name as warehouse,
+	h.datecreated + (nullif(h.timecreated, '')::DECIMAL(7,5) || ' hours')::interval as created_at,
+	cu.full_name as created_by,
+	h.dateupdated + (nullif(h.timeupdated, '')::DECIMAL(7,5) || ' hours')::interval as updated_at,
+	uu.full_name as updated_by
+from po_receipthistorydetail d
+join {{source('sage','po_receipthistoryheader')}} h on h.purchaseorderno = d.purchase_order_number and h.headerseqno = d.headerseqno and h.receiptno = d.receipt_number
+left join {{ref('sy_user')}} cu on cu.user_key = h.usercreatedkey
+left join {{ref('sy_user')}} uu on uu.user_key = h.userupdatedkey
+left join {{ref('im_warehouse')}} w on w.warehouse_code = d.warehouse_code
--- models/source/im-bm-ci/im_item_transactions.sql
+++ models/source/im-bm-ci/im_item_transactions.sql
@@ -1,19 +1,23 @@
 select
-entryno as entry_number,
-customerno as customer_code,
-itemcode as sku,
-warehousecode as warehouse_code,
-case transactiondate 
-	when '1753-01-01' then null
-	else transactiondate::date
-end as transaction_date, 
-case referencedate 
-	when '1753-01-01' then null
-	else referencedate::date
-end as sent_date, 
-transactionqty as transaction_quantity,
-transactioncode as transaction_type,
-dateupdated + (nullif(timeupdated, '')::DECIMAL(7,5) || ' hours')::interval as updated_at,
-uu.full_name as updated_by
+	entryno as entry_number,
+	customerno as customer_code,
+	itemcode as sku,
+	--Warehouse 038 and 049 both represent the same Garfield location. 038 is deprecated in favor of 049
+	case warehousecode 
+	  when '038' then '049' 
+	  else warehousecode 
+	end as warehouse_code,  
+	case transactiondate 
+		when '1753-01-01' then null
+		else transactiondate::date
+	end as transaction_date, 
+	case referencedate 
+		when '1753-01-01' then null
+		else referencedate::date
+	end as sent_date, 
+	transactionqty as transaction_quantity,
+	transactioncode as transaction_type,
+	dateupdated + (nullif(timeupdated, '')::DECIMAL(7,5) || ' hours')::interval as updated_at,
+	uu.full_name as updated_by
 from {{source('sage','im_itemtransactionhistory')}} t
 left join {{ref('sy_user')}} uu on uu.user_key = userupdatedkey
