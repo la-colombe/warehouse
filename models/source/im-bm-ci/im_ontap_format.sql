f3ee0a1634f3f78ae621308ca7891917de1d47e4
chris-schmid <chris.schmid@lacolombe.net>
2024-01-25 10:50:39 -0500

    Update im_ontap_format.sql

--- models/source/im-bm-ci/im_ontap_format.sql
+++ models/source/im-bm-ci/im_ontap_format.sql
@@ -3,6 +3,6 @@ select
     udf_format_desc as format_desc,
     nullif(udf_liquid_ounces, '')::int as liquid_ounces,
     nullif(udf_sub_unit_format, '')::int as sub_unit_format_id,
-    nullif(udf_units_in_pack, '')::int as units_in_pack
+    nullif(udf_units_in_pack, '')::int as units_per_pack
 from {{source('sage','im_udt_ontap_format')}}
 
