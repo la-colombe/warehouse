70a413a8e64bd1504ad62c83be565ab627e8bebc
AuggDawgLC <augustine.vitacolonna@lacolombe.net>
2024-01-25 11:03:04 -0500

    changes to upp and productline

--- models/source/im-bm-ci/im_productline.sql
+++ models/source/im-bm-ci/im_productline.sql
@@ -2,6 +2,10 @@ select
     productline as product_line,
     productlinedesc as product_line_desc,
     producttype as product_type,
-    udf_product_type,
-    udf_type
+    inventoryacctkey as inventory_gl_id,
+    costofgoodssoldacctkey as cogs_gl_id,
+    salesincomeacctkey as sales_income_gl_id,
+    returnsacctkey as return_gl_id,
+    adjustmentacctkey as adjustment_gl_id,
+    purchaseacctkey as purchase_gl_id
 from {{source('sage', 'im_productline')}}
